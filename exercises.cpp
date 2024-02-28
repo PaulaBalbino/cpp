7c162fd73246ed4ac14a667f7fa95e39e469ec7c
Paula Lucena Balbino <116719147+PaulaBalbino@users.noreply.github.com>
2024-02-28 22:25:35 +0400

    Add files via upload

---
+++ exercises.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+/*
+
+//BMI CALCULATOR
+
+int main()
+{
+	//underweight < 18.5
+	// normal weight  18.5 - 24.5
+	// overweight > 25
+
+	float weight;
+	float height;
+	float bmi;
+
+	cout << "Enter your weight: ";
+	cin >> weight;
+	cout << "Enter your heigh: ";
+	cin >> height;
+	bmi = weight / (height*height);
+
+	if (bmi < 18.5)
+	cout << "underweight" << endl;
+
+	else if (bmi > 25)
+			cout << "overweight" << endl;
+	else
+			cout << "normal" << endl;
+
+cout << "Your BMI is " << bmi;
+}
+*/
+
+/*
+int main()
+{
+	int nb;
+
+	cout << "Enter whole number: ";
+	cin >> nb;
+	if (nb % 2 == 0)
+		cout << "Even number" << endl;
+	else
+		cout << "Odd number" << endl;
+}
+*/
+
+/*
+int main()
+{
+	char c1, c2, c3, c4, c5;
+	cout << "Enter 5 letters: ";
+	cin >> c1 >> c2 >> c3 >> c4 >> c5;
+	cout << "ASCII message: " << int(c1) << " " << int(c2) << " "
+		<< int(c3) << " " << int(c4) << " " << int(c5);
+
+}
+*/
+
+/*
+int main()
+{
+	//ASCII
+
+	cout << (int)'a' << endl;
+	cout << int('a') << endl;
+	cout << int('A') << endl;
+
+	cout << char(65) << endl;
+
+}
+*/
+
+/*
+int main()
+{
+int yearBirth = 1989;
+char gender = 'f';
+bool isOlderThan18 = true;
+float averageGrade = 4.5;
+double balance = 76543298; // double size of float
+
+cout << "Size of this int is " <<sizeof(int) << " bytes\n";
+cout << "Int min value is " << INT_MIN << endl;
+cout << "Int max value is " << INT_MAX << endl;
+
+cout << "Size of unsigned int is " << sizeof(unsigned int) << " bytes\n"; //UInt
+
+}
+*/
+
+/*
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float annualSalary;
+	cout<<"Please enter your annual salary: ";
+	cin>>annualSalary;
+	float monthlySalary = annualSalary / 12;
+	cout << "Your monthly salary is " << monthlySalary <<endl;
+	cout << "In ten years you will earn " << annualSalary*10;
+
+	system("pause>0");
+
+}
+*/
+
+/*
+
+	cout << "hello dev\n";
+	cout << "My name is Paula"<<endl;
+	cout << "Today is Wednesday\n";
+	system("pause>0");
+
+
+int main()
+{
+	std::cout << "hello dev\n";
+	std::cout << "My name is Paula"<<std::endl;
+	std::cout << "Today is Wednesday\n";
+	system("pause>0");
+}
+*/
